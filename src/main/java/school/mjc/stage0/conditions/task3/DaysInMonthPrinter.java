c3fc6d5c25c446be13c0d4eed3bf67b28a827701
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-23 22:13:51 +0400

    Update

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,27 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month >= 1 && month <= 12) {
+            int[] daysInMonth = {
+                    31, // January
+                    28, // February
+                    31, // March
+                    30, // April
+                    31, // May
+                    30, // June
+                    31, // July
+                    31, // August
+                    30, // September
+                    31, // October
+                    30, // November
+                    31  // December
+            };
+
+            int days = daysInMonth[month - 1];
+            System.out.println(+ month  + days);
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
+
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            double netSalary = salary * 0.85;
+            System.out.println( netSalary);
+        } else if (salary <= 20000) {
+            double netSalary = salary * 0.82;
+            System.out.println( netSalary);
+        } else {
+            double netSalary = salary * 0.8;
+            System.out.println( netSalary);
+        }
     }
 }
+
